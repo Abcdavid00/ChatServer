38c970b7b4ffca3b9541db97dffa66857e6aab18
Abcdavid00 <83356700+Abcdavid00@users.noreply.github.com>
2023-06-20 19:35:50 +0700

    Added socket interface for sending message

--- server/src/sockets/index.js
+++ server/src/sockets/index.js
@@ -1,9 +1,18 @@
 import { Server } from 'socket.io';
 import BiMap from 'bidirectional-map';
 import { socketLogger, combinedLogger } from '../utils/logger.js';
+import { getUsers } from '../utils/roomCache.js';
 
 let io; // Store the Socket.IO instance
 let onlineUsers // Store the online users
+
+function getUserId(socketId) {
+    return onlineUsers.getKey(socketId);
+}
+
+function getSocketId(userId) {
+    return onlineUsers.get(userId);
+}
 
 export function initializeSocket(server) {
     io = new Server(server);
@@ -16,6 +25,8 @@ export function initializeSocket(server) {
 
         let sid = socket.id;
         let uid = null
+
+        socket.emit('request user');
 
         socket.on('set user', (id) => {
             socketLogger.info(`Client ${socket.id} set user: ${id}`);
@@ -39,4 +50,26 @@ export function getIO() {
         throw new Error('Socket.IO has not been initialized');
     }
     return io;
+}
+
+async function OnClientChat(socket, data) {
+    const { roomId, content } = data;
+    if (!roomId || !content) {
+        socketLogger.error(`Invalid message from client ${socket.id}: ${JSON.stringify(data)}`);
+        return;
+    }
+    const senderId = getUserId(socket.id);
+    try {
+        const message = await createMessage(roomId, senderId, content);
+        const users = await getUsers(roomId);
+        users.forEach((user) => {
+            const socketId = getSocketId(user._id);
+            if (socketId) {
+                io.to(socketId).emit('chat message', message);
+            }
+        })
+        socketLogger.info(`User ${senderId} sent message to room ${roomId}: ${content}`);
+    } catch (error) {
+        socketLogger.error(`Error while sending message to room ${roomId}: ${error}`);
+    }
 }
