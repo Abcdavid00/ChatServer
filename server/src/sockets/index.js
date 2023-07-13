f01ae84f64162acfda8af191ebe5586f8d34404c
Abcdavid00 <83356700+Abcdavid00@users.noreply.github.com>
2023-07-14 01:59:15 +0700

    Print room Obj

--- server/src/sockets/index.js
+++ server/src/sockets/index.js
@@ -92,6 +92,7 @@ async function OnClientSendMessage(socket, data) {
 
 async function OnRoomCreated(room) {
     const users = await getRoomUsers(room._id);
+    console.log("Room created: ", JSON.stringify(room))
     console.log(`Users in room ${room._id}: `, users)
     for (const user of users) {
         const socketId = getSocketId(user);
